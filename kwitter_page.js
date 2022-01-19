ab43233ff7266908b74de9fb3f945d4d4ee98544
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-01-19 19:52:22 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -17,7 +17,9 @@ document.getElementById("roomnameint").innerHTML = room_name;
 function send() {
       msg = document.getElementById("msg").value;
       firebase.database().ref(room_name).push({
-            name:username, message:msg, like:0
+            name: username,
+            message: msg,
+            like: 0
       });
       document.getElementById("msg").value = "";
 }
@@ -34,14 +36,31 @@ function getData() {
             snapshot.forEach(function (childSnapshot) {
                   childKey = childSnapshot.key;
                   childData = childSnapshot.val();
-                  if (childKey != "purpose") {
+                  if (childKey != "Purpose") {
                         firebase_message_id = childKey;
                         message_data = childData;
-                        //Start code
-
-                        //End code
+                        console.log(firebase_message_id);
+                        console.log(message_data);
+                        user_name = message_data['name'];
+                        message = message_data['message'];
+                        like = message_data['like'];
+                        name_tag = "<h4>"+user_name+ "<img class='user_tick' src='tick.png'></h4>";
+                        message_tag = "<h4 class='message_h4'>"+message+"</h4>";
+                        like_btn = "<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updatelike(this.id)'>";
+                        span_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like:"+like+"</span></button><hr>";
+                        row = name_tag+message_tag+like_btn+span_tag;
+                        document.getElementById("output").innerHTML += row;
                   }
             });
       });
 }
 getData();
+
+function updatelike(message_id) {
+      console.log(message_id);
+      newlike = document.getElementById(message_id).value;
+      updatedlike = Number(newlike)+1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like:updatedlike
+      });
+}
